88b65f85f143afdff27b83b6f8a3de91855eafb5
yargy <yargy@hotmail.com>
2013-10-09 23:35:23 +1100

    Added frontpage template file. Added front controller to index.php to handle all requests.

--- index.php
+++ index.php
@@ -1,2 +1,22 @@
-<?php include('template/header.php'); ?>
-<?php include('template/footer.php'); ?>
+<?php
+
+if (isset($_GET['page']) === FALSE) {
+  $request = 'frontpage';
+} else {
+  $request = $_GET['page'];
+}
+
+switch ($request) {
+
+case 'frontpage':
+  include('template/header.php');
+  include('template/front.php');
+  include('template/footer.php');
+  break;
+
+default:
+  // 404 stuff
+  include('template/header.php');
+  include('template/404.php');
+  include('template/footer.php');
+}
